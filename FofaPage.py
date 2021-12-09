dbbf85764f0119aae8eb9fd358902b0b0faa4a11
XD <57057346+Xd-tl@users.noreply.github.com>
2021-12-09 22:12:20 +0800

    Update FofaPage.py

--- FofaPage.py
+++ FofaPage.py
@@ -8,7 +8,9 @@
 
 '''
 import argparse
+import ast
 import base64
+import json
 import re
 import time
 from xlrd import open_workbook
@@ -16,8 +18,11 @@ from xlutils.copy import copy
 import requests
 import xlwt
 
-headers = {}
 fields = ['domain', 'host', 'ip', 'port', 'title', 'server', 'protocol', 'banner', 'city', 'region']
+headers = {}
+page = 0
+fragile = []
+
 
 def logo():
     print("""
@@ -38,6 +43,8 @@ def logo():
                                                             Author : xd
                                                              CSDN  : https://blog.csdn.net/xd_2021
           """)
+
+
 def excel(path):
     f = xlwt.Workbook()
     Sheet = f.add_sheet(u'Sheet', cell_overwrite_ok=True)  # 创建sheet
@@ -68,6 +75,27 @@ def To_excel(list, path):
         return 1
 
 
+def get_shengji(str, num):
+    list = []
+    try:
+        r = requests.get(url="http://xzqh.mca.gov.cn/map", timeout=5, verify=False)
+        shengji = re.search(r'value=\'\[\{.*\}\]', r.text).group().split('\'')
+        shengji = json.loads(shengji[1])
+        code = ''
+        for i in shengji:
+            if str in i['cName']:
+                code = i['code']
+                break
+        num += 3
+        code = code[:num]
+        for i in shengji:
+            if code in i['code'][:num]:
+                list.append(i['cName'])
+        return list
+    except Exception:
+        pass
+
+
 # 获取某一页数据
 def get(be64, page, filename):
     url = "https://api.fofa.so/v1/search?qbase64=%s&full=false&pn=%s&ps=10" % (be64.decode(), page)
@@ -77,8 +105,8 @@ def get(be64, page, filename):
         r = requests.get(url=url, headers=headers, timeout=10)
         data = r.json()['data']['assets']
         if data == []:
-            f.write('\n--- There are %s pages in all ---\n' % page)
-            print('\n--- There are %s pages in all ---' % page)
+            f.write('\n--- There are %s pages in all ---\n' % (page - 1))
+            print('\n--- There are %s pages in all ---' % (page - 1))
             f.close()
             return 1
         for i in data:
@@ -88,9 +116,9 @@ def get(be64, page, filename):
             excel_list.append(list)
     except Exception:
         query = base64.b64decode(be64)
-        f.write('\n--- %s Exception request ---\n' % query.decode())
+        f.write('\n--- %s Load page 11 exception ---\n' % page)
         f.close()
-        print('\n--- %s Exception request ---' % query.decode())
+        print('\n--- %s Load page 11 exception ---' % page)
         return -1
     if To_excel(excel_list, filename) == 0:
         f.write('[+] Page %s loads into Excel\n' % page)
@@ -103,7 +131,7 @@ def get(be64, page, filename):
 
 
 # 查询
-def rule(query, page):
+def rule(query):
     query_base64 = base64.b64encode(query.encode(encoding='utf-8'))
     filename = '%s' % int(time.time())
     excel(filename)
@@ -116,6 +144,31 @@ def rule(query, page):
             break
 
 
+def asset(city, province):
+    global page
+    parameter = get_shengji(city, province)
+    print("Looking for vulnerable targets")
+    print(fragile)
+    print("The city list:")
+    print(parameter)
+    filename = '%s' % int(time.time())
+    excel(filename)
+    for parameter1 in parameter:
+        for parameter2 in fragile:
+            query = 'title="%s"&&title="%s"&&country="CN"&&region!="HK"' % (parameter1, parameter2)
+            f = open('./log/%s.log' % filename, 'a')
+            f.write("query: %s\n\n" % query)
+            f.close()
+            print("query: %s\n" % query)
+            query_base64 = base64.b64encode(query.encode(encoding='utf-8'))
+            val = 0
+            for i in range(1, page):
+                if get(query_base64, i, filename) != 0:
+                    val = i
+                    break
+            page -= val - 1
+
+
 def main():
     logo()
     parser = argparse.ArgumentParser(description='Fofa Crawl by page number.')
@@ -125,17 +178,50 @@ def main():
         type=str,
         help="Please enter a query statement. e.g: -q \"app=\'Landray-OA系统\'&&country=\'CN\'\"",
         metavar='')
+    parser.add_argument(
+        "-c",
+        "--city",
+        type=str,
+        help="Please enter a query statement. e.g: -c \"city\"",
+        metavar='')
+    parser.add_argument(
+        "-p",
+        "--province",
+        type=int,
+        help="0 is province, 1 is city,default=True e.g: -p 0",
+        default=0,
+        metavar='')
     args = parser.parse_args()
+
+    # 获取用户输入
     query = args.query
-    query = query.replace("'", "\"")
-    config = open('config').read().split('\n')
+    city = args.city
+    province = args.province
+
+    # 获取config配置
+    global page
+    global fragile
+
+    config = open('config', encoding='utf-8').read().split('\n')
     pattern = re.compile('fofa_token=.*?;')
     Authorization = pattern.search(config[0])[0][:-1].split('=')[1]
     Size = config[1].split('=')[1]
     headers['Authorization'] = Authorization
     page = int(int(Size) / 10) + 1
     page = page if int(Size) % 10 == 0 else page + 1
-    rule(query, page)
+    fragile = config[2].split('=')[1]
+    fragile = ast.literal_eval(fragile)
+
+    if not query is None and not city is None:
+        print("q and c cannot be used at the same time.....")
+        return
+    elif not query is None:
+        query = query.replace("'", "\"")
+        rule(query)
+    elif not city is None:
+        asset(city, province)
+    else:
+        print("syntax error....")
 
 
 if __name__ == "__main__":
